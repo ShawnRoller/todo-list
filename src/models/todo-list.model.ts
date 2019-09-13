3cd5305b9a5b5a3372fbdb1ebeb9ca4d9b061735
SRoller <sroller@justfab.com>
2019-09-13 14:30:53 -0400

    fixes minor double-import issue

--- src/models/todo-list.model.ts
+++ src/models/todo-list.model.ts
@@ -1,5 +1,4 @@
-import {Entity, model, property} from '@loopback/repository';
-import {hasMany} from '@loopback/repository';
+import {Entity, model, property, hasMany} from '@loopback/repository';
 import {Todo, TodoWithRelations} from './todo.model';
 
 @model({settings: {}})
